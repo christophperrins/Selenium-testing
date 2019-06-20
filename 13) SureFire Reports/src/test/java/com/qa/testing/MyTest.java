fe4b4e04e7931e6fd1b377a9f9e1fad8a59ed7df
Chris Perrins <christopher.perrins@qa.com>
2019-06-20 10:01:26 +0100

    surefire reports

---
+++ 13) SureFire Reports/src/test/java/com/qa/testing/MyTest.java
@@ -0,0 +1,18 @@
+package com.qa.testing;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test1() {
+		
+	}
+	
+	@Test
+	public void test2() {
+		fail();
+	}
+}
